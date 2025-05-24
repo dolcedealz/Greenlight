f6db25ac8b8ca1a0b1fd34f59a79224695b1e583
dolcedealz <akinitovfilipp@gmail.com>
2025-05-24 20:08:01 +0300

    Update index.js 28

---
+++ frontend/src/components/games/crash/index.js
@@ -0,0 +1,6 @@
+// frontend/src/components/games/crash/index.js
+export { default as CrashGame } from './CrashGame';
+export { default as CrashGraph } from './CrashGraph';
+export { default as CrashControls } from './CrashControls';
+export { default as CrashBetsList } from './CrashBetsList';
+export { default as CrashHistory } from './CrashHistory';

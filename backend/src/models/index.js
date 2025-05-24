cb2671698c50ea78ab7858fe29a629ef95c693d2
dolcedealz <akinitovfilipp@gmail.com>
2025-05-24 20:27:16 +0300

    Update index.js 33

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,11 +2,13 @@
 const User = require('./user.model');
 const Game = require('./game.model');
 const Transaction = require('./transaction.model');
-const Deposit = require('./deposit.model'); // Добавляем модель депозитов
+const Deposit = require('./deposit.model');
+const CrashRound = require('./crash-round.model'); // ДОБАВЛЯЕМ
 
 module.exports = {
   User,
   Game,
   Transaction,
-  Deposit
+  Deposit,
+  CrashRound // ДОБАВЛЯЕМ
 };

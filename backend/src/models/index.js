19a994d242623bb51a404372e32b58c8f9142c01
dolcedealz <akinitovfilipp@gmail.com>
2025-05-24 14:11:39 +0200

    oplata

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,10 +1,12 @@
-// index.js
+// backend/src/models/index.js
 const User = require('./user.model');
 const Game = require('./game.model');
 const Transaction = require('./transaction.model');
+const Deposit = require('./deposit.model'); // Добавляем модель депозитов
 
 module.exports = {
   User,
   Game,
-  Transaction
+  Transaction,
+  Deposit
 };

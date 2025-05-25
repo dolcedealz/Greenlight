9717fb2d51983391fd9eea1d31b024fdf1aae42a
dolcedealz <akinitovfilipp@gmail.com>
2025-05-25 04:05:08 +0300

    Update index.js 777

--- frontend/src/components/games/coin/index.js
+++ frontend/src/components/games/coin/index.js
@@ -1,3 +1,4 @@
-// index.js
+// frontend/src/components/games/coin/index.js
 export { default as CoinFlip } from './CoinFlip';
 export { default as CoinControls } from './CoinControls';
+export { default as CoinGame } from './CoinGame';

0b6053e1486aca2fa36eec5ecd9af3711827da36
dolcedealz <akinitovfilipp@gmail.com>
2025-05-27 19:25:35 +0300

    Update GameBlock.js тактиле

--- frontend/src/components/main/GameBlock.js
+++ frontend/src/components/main/GameBlock.js
@@ -1,10 +1,22 @@
-// GameBlock.js
+// frontend/src/components/main/GameBlock.js
 import React from 'react';
+import useTactileFeedback from '../../hooks/useTactileFeedback';
 import '../../styles/GameBlock.css';
 
 const GameBlock = ({ name, icon, onClick }) => {
+  const { gameActionFeedback } = useTactileFeedback();
+
+  const handleClick = () => {
+    // Вибрация при выборе игры
+    gameActionFeedback();
+    
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <div className="game-block" onClick={onClick}>
+    <div className="game-block" onClick={handleClick}>
       <div className="game-icon">{icon}</div>
       <div className="game-name">{name}</div>
     </div>

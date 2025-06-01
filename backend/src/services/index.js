660dc6473dc66c121f7f4b675e8437289ee9325d
dolcedealz <akinitovfilipp@gmail.com>
2025-06-01 16:19:59 +0300

    Update index.js рр3

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,28 +1,26 @@
 // backend/src/services/index.js - ОБНОВЛЕННАЯ ВЕРСИЯ
-const randomService = require('./random.service');
 const gameService = require('./game.service');
 const userService = require('./user.service');
+const adminService = require('./admin.service');
 const paymentService = require('./payment.service');
 const withdrawalService = require('./withdrawal.service');
-const oddsService = require('./odds.service');
-const casinoFinanceService = require('./casino-finance.service');
+const financeService = require('./finance.service');
 const referralService = require('./referral.service');
 const crashService = require('./crash.service');
 const authService = require('./auth.service');
 const pvpService = require('./pvp.service');
-const eventService = require('./event.service'); // Добавляем события
+const eventService = require('./event.service'); // Добавляем сервис событий
 
 module.exports = {
-  randomService,
   gameService,
   userService,
+  adminService,
   paymentService,
   withdrawalService,
-  oddsService,
-  casinoFinanceService,
+  financeService,
   referralService,
   crashService,
   authService,
   pvpService,
-  eventService // Добавляем события
+  eventService // Добавляем сервис событий
 };

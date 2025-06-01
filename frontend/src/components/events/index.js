de0633d9a9a5c3a5834712fd75d2f64eb0a44158
dolcedealz <akinitovfilipp@gmail.com>
2025-06-01 12:33:29 +0300

    Update index.js уъ2

---
+++ frontend/src/components/events/index.js
@@ -0,0 +1,4 @@
+// frontend/src/components/events/index.js
+export { default as EventCard } from './EventCard';
+export { default as EventDetails } from './EventDetails';
+export { default as EventBet } from './EventBet';

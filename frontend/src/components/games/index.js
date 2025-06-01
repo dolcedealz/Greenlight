7a80b3af3e02a94cf75f3a0a99f8a3a51228ed01
dolcedealz <akinitovfilipp@gmail.com>
2025-06-01 15:36:50 +0300

    Update index.js уъу12

---
+++ frontend/src/components/games/index.js
@@ -0,0 +1,5 @@
+// frontend/src/components/games/index.js
+export { default as CoinGame } from './coin/CoinGame';
+export { default as MinesGame } from './mines/MinesGame';
+export { default as SlotGame } from './slots/SlotGame';
+export { default as CrashGame } from './crash/CrashGame';

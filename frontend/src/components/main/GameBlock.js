a5e9e804a4947b71d867e83d8082dc88a55ca318
dolcedealz <akinitovfilipp@gmail.com>
2025-06-03 22:29:12 +0300

    Update GameBlock.js skyfall4

--- frontend/src/components/main/GameBlock.js
+++ frontend/src/components/main/GameBlock.js
@@ -1,10 +1,14 @@
-// frontend/src/components/main/GameBlock.js
+// frontend/src/components/main/GameBlock.js - ОБНОВЛЕННАЯ ВЕРСИЯ
 import React from 'react';
 import '../../styles/GameBlock.css';
 
-const GameBlock = ({ name, icon, onClick }) => {
+const GameBlock = ({ name, icon, onClick, gameType }) => {
   return (
-    <div className="game-block" onClick={onClick}>
+    <div 
+      className="game-block" 
+      onClick={onClick}
+      data-game={gameType}
+    >
       <div className="game-icon">{icon}</div>
       <div className="game-name">{name}</div>
     </div>
